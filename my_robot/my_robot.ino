a1389a6236c5ab4267389c91c925cd58a940ce2f
kdsystem <kdsystem@gmail.com>
2016-12-28 10:03:09 +0300

    2nd release
    2 dc's must be connected to 3,4 pins at l293 shield because 2&3 pins on
    uno are for interrupts

--- my_robot/my_robot.ino
+++ my_robot/my_robot.ino
@@ -1,14 +1,24 @@
-// Тестировалось на Arduino IDE 1.0.1
 #include <AFMotor.h>  // Подключаем библиотеку для работы с шилдом 
 #include <Cyberlib.h>
 //Управление пинами:
-// считаем, что motor1 - левый мотор
-// считаем, что motor2 - правый мотор
+// считаем, что motorL - левый мотор
+// считаем, что motorR - правый мотор
 //Dx_Out;  - установка пина Х как выход
 //Dx_In; - установка пина Х  как вход
 //Dx_Hihg;-  установка высокого уровна на пине Х
 //Dx_Low; - установка низкого уровня на пине Х
 //Ax_Read; - чтение аналогового пина Х
+//Выводы, отвечающие за скорость вращения двигателей
+//Цифровой вывод 11- DC Мотор №1 / Шаговый №1
+//Цифровой вывод 3- DC Мотор №2 / Шаговый №1
+//Цифровой вывод 5- DC Мотор №3 / Шаговый №2
+//Цифровой вывод 6- DC Мотор №4 / Шаговый №2
+//Выводы, отвечающие за выбор направления вращения двигателей:
+//Цифровые выводы 4, 7, 8 и 12
+//Выводы для управления сервоприводами (выведены на штырьки на краю платы):
+//Цифровой вывод 9- Сервопривод №1
+//Цифровой вывод 10- Сервопривод №2
+
 #define encoders_init D2_In; D3_In; D2_High; D3_High
 //#define robot_go D4_Low; D5_High; D6_High; D7_Low
 //#define robot_stop D4_Low; D5_Low; D6_Low; D7_Low
@@ -17,17 +27,16 @@
 #define robot_rotation_left D4_Low; D5_High; D6_Low; D7_High
 
 volatile int8_t course=0;
-// Подключаем моторы к клеммникам M1, M2, M3, M4
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
+// Подключаем моторы к клеммникам M3, M4
+AF_DCMotor motorL(3);
+AF_DCMotor motorR(4);
 
 void setup() {
   IntOn();  //включить прерывания
-  // Задаем максимальную скорость вращения моторов (аналог работы PWM) 
-  motor1.setSpeed(155);
-  motor2.setSpeed(155);
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
+  motorL.setSpeed(155);
+  motorR.setSpeed(155);
+  motorL.run(RELEASE);
+  motorR.run(RELEASE);
 }
 
 int i;
@@ -37,16 +46,16 @@ void Rotation()
   IntOff();
   robot_rotation_left;
   delay_ms(180);
-  motor1.run(RELEASE);
-  motor2.run(RELEASE); 
+  motorL.run(RELEASE);
+  motorR.run(RELEASE); 
   course=0; 
   IntOn();  
 }   
 
 void IntOn()
 {
-  attachInterrupt(0, MotorL, RISING);  // настроить срабатывание прерывания interrupt0 pin 2 по перерпапду с низкого (Low) на высокий(HIGH)
-  attachInterrupt(1, MotorR, RISING);  // настроить срабатывание прерывания interrupt1 pin 3 по перерпапду с низкого (Low) на высокий(HIGH) 
+  attachInterrupt(0, left_interrupt, RISING);  // настроить срабатывание прерывания interrupt0 pin 2 по перепаду с низкого (Low) на высокий(HIGH)
+  attachInterrupt(1, right_interrupt, RISING);  // настроить срабатывание прерывания interrupt1 pin 3 по перепаду с низкого (Low) на высокий(HIGH) 
 }
 
 void IntOff()
@@ -58,40 +67,42 @@ void IntOff()
 void robot_left()
 {
   IntOff();
-  motor1.setSpeed(55);
-  motor2.setSpeed(55);
+  motorL.setSpeed(155);
+  motorR.setSpeed(155);
   //остановили моторы
-  motor1.run(RELEASE); 
-  motor2.run(RELEASE);
+  motorL.run(RELEASE); 
+  motorR.run(RELEASE);
   //поворачиваем правое назад, левое вперед на 180 милисек
-  motor1.run(BACKWARD);
-  motor2.run(FORWARD);
-  delay(180);
+  motorL.run(BACKWARD);
+  motorR.run(FORWARD);
+  delay(1800);
   IntOn();
 }
 
 void robot_right()
 {
   IntOff();
-  motor1.setSpeed(55);
-  motor2.setSpeed(55);
+  motorL.setSpeed(155);
+  motorR.setSpeed(155);
   //остановили моторы
-  motor1.run(RELEASE); 
-  motor2.run(RELEASE);
+  motorL.run(RELEASE); 
+  motorR.run(RELEASE);
   //поворачиваем правое вперед, левое назад на 180 милисек
-  motor1.run(FORWARD);
-  motor2.run(BACKWARD);
-  delay(180);
+  motorL.run(FORWARD);
+  motorR.run(BACKWARD);
+  delay(1800);
   IntOn();
 }
 
-void  MotorL()  // обработка внешнего прерывания левое колесо
+void  left_interrupt()  
+// обработка внешнего прерывания левое колесо
 { 
   course--; 
   if(course > 0) { robot_left(); }else{ robot_right();}
 }  
 
-void  MotorR()  // обработка внешнего прерывания правое колесо
+void  right_interrupt()  
+// обработка внешнего прерывания правое колесо
 {
    course++; 
    if(course < 0){ robot_right(); }else{ robot_left();}
@@ -99,15 +110,15 @@ void  MotorR()  // обработка внешнего прерывания пр
 
 void loop() {
   // Двигаемся условно вперед одну секунду 
-  motor1.run(FORWARD); // Задаем движение вперед
-  motor2.run(FORWARD);
-  motor1.setSpeed(155); // Задаем скорость движения
-  motor2.setSpeed(155); 
-  delay(1000);
+  motorL.run(FORWARD); // Задаем движение вперед
+  motorR.run(FORWARD);
+  motorL.setSpeed(155); // Задаем скорость движения
+  motorR.setSpeed(155); 
+  delay(5000);
   
   // Останавливаем двигатели
-  motor1.run(RELEASE); 
-  motor2.run(RELEASE);
+  motorL.run(RELEASE); 
+  motorR.run(RELEASE);
   delay(500);
   
   //поворот налево
