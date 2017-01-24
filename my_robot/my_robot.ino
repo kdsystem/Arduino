352cb9ab7899372faf8e38373b1f26e9a8fcc454
kdsystem <kdsystem@gmail.com>
2017-01-24 10:57:34 +0300

    3.2.2
    добавляю ик модули и обработчики

--- my_robot/my_robot.ino
+++ my_robot/my_robot.ino
@@ -19,16 +19,21 @@
 //Цифровой вывод 9- Сервопривод №1
 //Цифровой вывод 10- Сервопривод №2
 
-#define encoders_init D2_In; D3_In; D2_High; D3_High
+//#define encoders_init D2_In; D3_In; D2_High; D3_High
 //#define robot_go D4_Low; D5_High; D6_High; D7_Low
 //#define robot_stop D4_Low; D5_Low; D6_Low; D7_Low
 //#define robot_left  D4_Low; D5_High; D6_Low; D7_Low
 //#define robot_right D4_Low; D5_Low; D6_High; D7_Low
-#define robot_rotation_left D4_Low; D5_High; D6_Low; D7_High
+//#define robot_rotation_left D4_Low; D5_High; D6_Low; D7_High
+#define ir_l A0 //пин 1 ИК-датчика
+#define ir_ml A1 //пин 2 ИК-датчика
+#define ir_mr A2 //пин 3 ИК-датчика
+#define ir_r A3 //пин 4 ИК-датчика
+String triggered_ir;
 
 const byte MOTOR_SPEED = 80; //скорость передвижения робота
 const byte TURN_SPEED = 80;
-const int TURN_TIME = 10;
+const int TURN_TIME = 100;
 int i;
 
 volatile int8_t course=0;
@@ -37,18 +42,23 @@ AF_DCMotor motorL(3);
 AF_DCMotor motorR(4);
 
 void setup() {
+  //звуковое оповещение готовности робота
+  //for(uint8_t i=0; i<12; i++) beep(70, random(100, 2000));
   //Serial.begin(9600);
   //pinMode(2,INPUT);
   //pinMode(3,INPUT);
   //motorL.setSpeed(MOTOR_SPEED);
   //motorR.setSpeed(MOTOR_SPEED);
+  pinMode(ir_l, INPUT);
+  pinMode(ir_ml, INPUT);
+  pinMode(ir_mr, INPUT);
+  pinMode(ir_r, INPUT);
   robot_stop();
   IntOn();  //включить прерывания
 }
 
 void IntOn()
 {
-  //было RISING, изменил на CHANGE
   attachInterrupt(0, left_interrupt, RISING);  // настроить срабатывание прерывания interrupt0 pin 2 по перепаду с низкого (Low) на высокий(HIGH)
   attachInterrupt(1, right_interrupt, RISING);  // настроить срабатывание прерывания interrupt1 pin 3 по перепаду с низкого (Low) на высокий(HIGH) 
 }
@@ -59,33 +69,104 @@ void IntOff()
   detachInterrupt(1);  // отключить срабатывание прерывания interrupt1
 }  
 
-void robot_right()
+void TurnLeft(String turn)
+{
+  if (turn == "left")
+  {
+    IntOff();
+    motorL.setSpeed(TURN_SPEED);
+    motorR.setSpeed(TURN_SPEED);
+    //поворачиваем левое назад
+    motorR.run(RELEASE);
+    motorL.run(BACKWARD);
+    delay(TURN_TIME);
+    //IntOn();          
+  }
+  if (turn == "middle_left")
+  {
+    IntOff();
+    motorL.setSpeed(TURN_SPEED);
+    motorR.setSpeed(TURN_SPEED);
+    //поворачиваем левое назад
+    motorR.run(RELEASE);
+    motorL.run(BACKWARD);
+    delay(TURN_TIME/2);
+    //IntOn();          
+  }
+}
+
+void TurnRight(String turn)
+{
+  if (turn == "right")
+  {
+    motorL.setSpeed(TURN_SPEED);
+    motorR.setSpeed(TURN_SPEED);
+    //поворачиваем левое назад
+    motorL.run(RELEASE);
+    motorR.run(BACKWARD);
+    delay(TURN_TIME);
+  }
+  if (turn == "middle_right")
+  {
+    motorL.setSpeed(TURN_SPEED);
+    motorR.setSpeed(TURN_SPEED);
+    //поворачиваем левое назад
+    motorL.run(RELEASE);
+    motorR.run(BACKWARD);
+    delay(TURN_TIME/2);
+  }
+}
+
+void getSensors()
+{
+  if (digitalRead(ir_l) == LOW)
+    {
+      triggered_ir="ir_l";
+      TurnLeft("left");
+      //Serial.println("Triggered ir is LEFT");
+    }
+
+  if (digitalRead(ir_ml) == LOW)
+    {
+      triggered_ir="ir_ml";
+      TurnLeft("middle_left");
+      //Serial.println("Triggered ir is MIDDLE LEFT");
+    }
+
+  if (digitalRead(ir_mr) == LOW)
+    {
+      triggered_ir="ir_mr";
+      TurnRight("right");
+      //Serial.println("Triggered ir is MIDDLE RIGHT");
+    }
+
+  if (digitalRead(ir_r) == LOW)
+    {
+      triggered_ir="ir_r";
+      TurnRight("middle_right");
+      //Serial.println("Triggered ir is RIGHT");
+    }
+}
+
+void correct_rigth()
 {
   IntOff();
-  //остановили моторы
-  //robot_stop();
   motorL.setSpeed(TURN_SPEED);
   motorR.setSpeed(TURN_SPEED);
-  //поворачиваем правое назад 1сек
-  //motorL.run(BACKWARD);
+  //поворачиваем правое назад
   motorL.run(RELEASE);
   motorR.run(BACKWARD);
-  //delay(TURN_TIME);
   IntOn();
 }
 
-void robot_left()
+void correct_left()
 {
   IntOff();
-  //остановили моторы
-  //robot_stop();
   motorL.setSpeed(TURN_SPEED);
   motorR.setSpeed(TURN_SPEED);
-  //поворачиваем левое назад 1сек
-  //motorL.run(FORWARD);
+  //поворачиваем левое назад
   motorR.run(RELEASE);
   motorL.run(BACKWARD);
-  //delay(TURN_TIME);
   IntOn();
 }
 
@@ -95,7 +176,8 @@ void  left_interrupt()
   course--;
   //Serial.print("Left interrupt......course "); 
   //Serial.println(course);   
-  if(course > 0) { robot_left(); }else{ robot_right();}
+  getSensors;
+  if(course > 0) { correct_left(); }else{ correct_rigth();}
 }  
 
 void  right_interrupt()  
@@ -104,7 +186,8 @@ void  right_interrupt()
    course++;
    //Serial.print("Right interrupt......course ");
    //Serial.println(course);   
-   if(course < 0){ robot_right(); }else{ robot_left();}
+   getSensors;
+   if(course < 0){ correct_rigth(); }else{ correct_left();}
 } 
 
 void robot_stop()
@@ -115,20 +198,10 @@ void robot_stop()
 }
 
 void loop() {
-  motorL.setSpeed(MOTOR_SPEED); // Задаем скорость движения
+  //Задаем скорость движения
+  motorL.setSpeed(MOTOR_SPEED);
   motorR.setSpeed(MOTOR_SPEED); 
-  // Двигаемся условно вперед пять секунд
-  motorL.run(FORWARD); // Задаем движение вперед
+  // Двигаемся вперед
+  motorL.run(FORWARD);
   motorR.run(FORWARD);
-  
-  //delay(5000);
-  
-  // Останавливаем двигатели
-  //robot_stop();
-  //delay(500);
-  
-  //поворот налево
-  //robot_left();
-  //delay_ms(500);
-  //robot_stop();
 }
