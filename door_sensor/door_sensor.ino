8bf9a0d6c785b3dcc46dfa723c4af7dab9cd09ca
kdsystem <kdsystem@gmail.com>
2017-04-24 13:23:55 +0300

    2.0
    sendind by raised interrupt from door switch

--- door_sensor/door_sensor.ino
+++ door_sensor/door_sensor.ino
@@ -1,12 +1,12 @@
 /**
- * Подключение:
- * SPI:
- * MISO -> 12
- * MOSI -> 11
- * SCK -> 13
- * CE -> 9
- * CSN -> 10
- */
+   Подключение:
+   SPI:
+   MISO -> 12
+   MOSI -> 11
+   SCK -> 13
+   CE -> 9
+   CSN -> 10
+*/
 
 #include <SPI.h>
 #include <Mirf.h>
@@ -21,50 +21,24 @@ void setup() {
   digitalWrite(switchPin, HIGH); // активируем внутренний подтягивающий резистор вывода
   pinMode(ledPin, OUTPUT); // задаём вывод 13 в качестве выхода
   Serial.begin(9600); // задействуем последовательный порт
+  //Инициализация передатчика
 
-  Mirf.csnPin = 10;
-  Mirf.cePin = 9;
-  Mirf.spi = &MirfHardwareSpi;
-  Mirf.init();
-  Mirf.setRADDR((byte *)"clie1");
-  Mirf.payload = sizeof(unsigned long);
-  Mirf.config();
-  Serial.println("Beginning ... ");
+  //Установим прерывание
+  attachInterrupt(switchPin, Send2HUB, CHANGE );
+}
+
+void Send2HUB() {
+  // Получаем количество миллисекунд, прошедших с момента включения платы:
+  unsigned long time = millis();
+  int doorstate = digitalRead(switchPin); // считываем показания с геркона
+  digitalWrite(ledPin, !doorstate); // инвертированные показания записываем в порт со светодиодом
+  //Отправить состояние двери (откр или закр)
+
 }
 
 void loop() {
-  // Получаем количество миллисекунд, прошедших с момента включения платы:
-  unsigned long time = millis();
-  // Устанавливаем адрес модуля. которому будем передавать пакет:
-  Mirf.setTADDR((byte *)"serv1");
-  // Передаём значение переменной time в виде массива байт:
-  Mirf.send((byte *) &time);
-
-  int g = digitalRead(switchPin); // считываем показания с геркона
-  digitalWrite(ledPin, !g); // инвертированные показания записываем в порт со светодиодом
-  Serial.println(g); // посылаем в последовательный порт значения с геркона
+  int doorstate = digitalRead(switchPin); // считываем показания с геркона
+  digitalWrite(ledPin, !doorstate); // инвертированные показания записываем в порт со светодиодом
+  Serial.println(doorstate); // посылаем в последовательный порт значения с геркона
   //когда геркон замкнут, значение "0" (LOW), когда разомкнут - "1" (HIGH)
-  delay(1000); // повторяем цикл через 1000 мсек
- 
-  // Ждём окончания передачи
-  while(Mirf.isSending()){
-  }
-  Serial.println("Finished sending");
-  delay(10);
-  // Если ничего не получено в ответ
-  while(!Mirf.dataReady()){
-    Serial.println("Waiting");
-    // В течении секунды
-    if ( ( millis() - time ) > 1000 ) {
-      // Считаем. что пакет потерян
-      Serial.println("Timeout on response from server!");
-      return;
-    }
-  }
-  // пробуем получить ответный пакет:
-  Mirf.getData((byte *) &time);
-  // Вычисляем задержку передачи
-  // и выводим полученное значение в серийный порт:
-  Serial.print("Ping: ");
-  Serial.println((millis() - time));
 }
