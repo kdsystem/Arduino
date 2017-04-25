fea6fb7cd7615f3f43bbbe948f1c2f2a8f31da7e
kdsystem <kdsystem@gmail.com>
2017-04-25 09:17:13 +0300

    2.1

--- door_sensor/door_sensor.ino
+++ door_sensor/door_sensor.ino
@@ -9,20 +9,28 @@
 */
 
 #include <SPI.h>
-#include <Mirf.h>
-#include <nRF24L01.h>
-#include <MirfHardwareSpiDriver.h>
+#include <RF24.h>
 
+// CE,CSN пины
+RF24 radio(9, 10);
+unsigned long count = 0;
 int switchPin = 2; // к выводу 2 подключён геркон
-int ledPin = 3; // к пину 13 подключён встроенный светодиод
+int ledPin = 3; // к пину 3 подключён cветодиод (13 - встроенный)
 
 void setup() {
   pinMode(switchPin, INPUT); // задаём вывод 2 в качестве входа (будем считывать с него)
   digitalWrite(switchPin, HIGH); // активируем внутренний подтягивающий резистор вывода
-  pinMode(ledPin, OUTPUT); // задаём вывод 13 в качестве выхода
+  pinMode(ledPin, OUTPUT); // задаём вывод 3 в качестве выхода
   Serial.begin(9600); // задействуем последовательный порт
-  //Инициализация передатчика
-
+  // Инициализируем канал связи и не только.
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x4c);
+  radio.openWritingPipe(0xf0f0f0f0d2LL);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
+  pinMode(switchPin, INPUT);
+  pinMode(ledPin, OUTPUT);
   //Установим прерывание
   attachInterrupt(switchPin, Send2HUB, CHANGE );
 }
@@ -30,10 +38,23 @@ void setup() {
 void Send2HUB() {
   // Получаем количество миллисекунд, прошедших с момента включения платы:
   unsigned long time = millis();
+  char outBuffer[32] = "";
   int doorstate = digitalRead(switchPin); // считываем показания с геркона
   digitalWrite(ledPin, !doorstate); // инвертированные показания записываем в порт со светодиодом
   //Отправить состояние двери (откр или закр)
-
+  //когда геркон замкнут, значение "0" (LOW), когда разомкнут - "1" (HIGH)
+  String out = "dev1:p1123123123:" + doorstate;
+  out.toCharArray(outBuffer, 32);
+  radio.write(outBuffer, 32);
+  delay(50);
+  // Не знаю почему, но периодически трансивер зависает. Помогает повторная инициализация.
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x4c);
+  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
+  delay(50);
 }
 
 void loop() {
