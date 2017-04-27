9578574f1c0f22f2f3ff02601689d516bde4546d
kdsystem <kdsystem@gmail.com>
2017-04-27 09:56:09 +0300

    1.0
    get self power
    get time from start

---
+++ self_power/get_self_power/get_self_power.ino
@@ -0,0 +1,33 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // задействуем последовательный порт
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  ADCSRA |= _BV(ADSC); // начало преобразований
+  while (bit_is_set(ADCSRA, ADSC)); // измерение
+  uint8_t low = ADCL; // сначала нужно прочесть ADCL - это запирает ADCH
+  uint8_t high = ADCH; // разлочить оба
+  float result = (high << 8) | low;
+  result = (1.1 * 1023.0 * 1000) / result; // Результат Vcc в милливольтах
+  Serial.println(result);
+  int time = millis() / 1000;
+  if (time / 60 / 60 < 10) {
+    Serial.print ("0");
+  }
+  Serial.print (time / 60 / 60);
+  Serial.print (":");
+  if (time / 60 % 60 < 10) {
+    Serial.print ("0");
+  }
+  Serial.print ((time / 60) % 60);
+  Serial.print (":");
+  if (time % 60 < 10) {
+    Serial.print ("0");
+  }
+  Serial.println (time % 60);
+  delay(1000);
+}
