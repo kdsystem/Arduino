0bf8d366938194073dd3d46eb05fa1d43f60abea
kdsystem <kdsystem@gmail.com>
2017-05-06 17:01:31 +0300

    1wire dallas
    data on pin 4

---
+++ 1wire_ds18b20/temperature_sensor/temperature_sensor.ino
@@ -0,0 +1,20 @@
+#include <OneWire.h>
+OneWire ds(4);
+void setup() {
+Serial.begin(9600); 
+}
+void loop() {
+byte data[2];
+ds.reset(); 
+ds.write(0xCC);
+ds.write(0x44);
+delay(750);
+ds.reset();
+ds.write(0xCC);
+ds.write(0xBE);
+data[0] = ds.read(); 
+data[1] = ds.read();
+int Temp = (data[1]<< 8)+data[0];
+Temp = Temp>>4;
+Serial.println(Temp);
+}
