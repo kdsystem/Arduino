7119376028fd2d0a30f1265a6cf35732a2e2c99c
kdsystem <kdsystem@gmail.com>
2017-05-06 12:20:47 +0300

    1.2
    with debounced signal from switch

--- door_sensor/door_sensor.ino
+++ door_sensor/door_sensor.ino
@@ -11,12 +11,12 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <stdio.h>
-
 // CE,CSN пины
 RF24 radio(9, 10);
 unsigned long count = 0;
 int switchPin = 2; // к выводу 2 подключён геркон
 int ledPin = 3; // к пину 3 подключён cветодиод (13 - встроенный)
+Bounce bouncer = Bounce(switchPin,5);
 
 void setup() {
   pinMode(switchPin, INPUT); // задаём вывод 2 в качестве входа (будем считывать с него)
@@ -39,25 +39,30 @@ void setup() {
 void Send2HUB() {
   // Получаем количество миллисекунд, прошедших с момента включения платы:
   unsigned long time = millis();
-  char outBuffer[32] = "";
-  delay(200);
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > 200) {
+    char outBuffer[32] = "";
+    delay(200);
   int doorstate = digitalRead(switchPin); // считываем показания с геркона
-  digitalWrite(ledPin, !doorstate); // инвертированные показания записываем в порт со светодиодом
-  Serial.println(doorstate);
-  //Отправить состояние двери (откр или закр)
-  //когда геркон замкнут, значение "0" (LOW), когда разомкнут - "1" (HIGH)
-  String out = "sensor1:switch1:" + String(doorstate);
-  out.toCharArray(outBuffer, 32);
-  radio.write(outBuffer, 32);
-  delay(50);
-  // Не знаю почему, но периодически трансивер зависает. Помогает повторная инициализация.
-  radio.begin();
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setChannel(0x4c);
-  radio.openWritingPipe(0xF0F0F0F0E1LL);
-  radio.enableDynamicPayloads();
-  radio.powerUp();
-  delay(50);
+    digitalWrite(ledPin, !doorstate); // инвертированные показания записываем в порт со светодиодом
+    Serial.println(doorstate);
+    //Отправить состояние двери (откр или закр)
+    //когда геркон замкнут, значение "0" (LOW), когда разомкнут - "1" (HIGH)
+    String out = "sensor1:switch1:" + String(doorstate);
+    out.toCharArray(outBuffer, 32);
+    radio.write(outBuffer, 32);
+    delay(50);
+    // Не знаю почему, но периодически трансивер зависает. Помогает повторная инициализация.
+    radio.begin();
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setChannel(0x4c);
+    radio.openWritingPipe(0xF0F0F0F0E1LL);
+    radio.enableDynamicPayloads();
+    radio.powerUp();
+    delay(50);
+ }
+ last_interrupt_time = interrupt_time;
 }
 
 void loop() {
