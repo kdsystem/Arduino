0de6e941e13b65de27de9f3f68a23e6e32f90b63
kdsystem <kdsystem@gmail.com>
2017-05-09 14:12:58 +0300

    1.4
    sending switch state, Vcc and temp from ds18b20

--- door_sensor/door_sensor.ino
+++ door_sensor/door_sensor.ino
@@ -11,8 +11,10 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <stdio.h>
+#include <OneWire.h>
 // CE,CSN пины
 RF24 radio(9, 10);
+OneWire ds(4);
 unsigned long count = 0;
 int switchPin = 2; // к выводу 2 подключён геркон
 int ledPin = 3; // к пину 3 подключён cветодиод (13 - встроенный)
@@ -41,16 +43,40 @@ void Send2HUB() {
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
   if (interrupt_time - last_interrupt_time > 200) {
-    char outBuffer[32] = "";
+    char outBuffer[40] = "";
     delay(200);
-  int doorstate = digitalRead(switchPin); // считываем показания с геркона
+    int doorstate = digitalRead(switchPin); // считываем показания с геркона
     digitalWrite(ledPin, !doorstate); // инвертированные показания записываем в порт со светодиодом
-    Serial.println(doorstate);
+    //Serial.println(doorstate);
+    //Узнаем напряжение питания платы
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    ADCSRA |= _BV(ADSC); // начало преобразований
+    while (bit_is_set(ADCSRA, ADSC)); // измерение
+    uint8_t low = ADCL; // сначала нужно прочесть ADCL - это запирает ADCH
+    uint8_t high = ADCH; // разлочить оба
+    float volt = (high << 8) | low;
+    volt = (1.1 * 1023.0 * 1000) / volt; // Результат Vcc в милливольтах
+
+    //Узнаем температуру DS18b20
+    byte data[2];
+    ds.reset(); 
+    ds.write(0xCC);
+    ds.write(0x44);
+    delay(750);
+    ds.reset();
+    ds.write(0xCC);
+    ds.write(0xBE);
+    data[0] = ds.read(); 
+    data[1] = ds.read();
+    int temp = (data[1]<< 8)+data[0];
+    temp = temp>>4;
+    
     //Отправить состояние двери (откр или закр)
     //когда геркон замкнут, значение "0" (LOW), когда разомкнут - "1" (HIGH)
-    String out = "sensor1:switch1:" + String(doorstate);
-    out.toCharArray(outBuffer, 32);
-    radio.write(outBuffer, 32);
+    String out = "s1|sw1:" + String(doorstate)+"|volt:"+volt/1000+"|temp:"+temp+"|";
+    Serial.println(out);
+    out.toCharArray(outBuffer, 40);
+    radio.write(outBuffer, 40);
     delay(50);
     // Не знаю почему, но периодически трансивер зависает. Помогает повторная инициализация.
     radio.begin();
